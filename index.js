cc6fcde00edfe6f61e524aca0a04eb0c4a8f87ac
Mayank Negi <mayank@Mayanks-MacBook-Air.local>
2024-01-17 21:40:42 +0530

    add /users endpoint

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
 const express = require('express')
 const app = express()
 
+const users = [
+    {
+        id: '1234',
+        name : 'John',
+        password: 'JJJJ',
+        phone: '99999112'
+    },
+    {
+        id: '2345',
+        name: 'Jim',
+        password: 'hello',
+        phone: '9091928'
+    }
+]
 app.get('/', (req, res) => {
     res.send('UP')
+})
+
+app.get('/users', (req, res) => {
+    res.json(users)
 })
 
 const port = process.env.PORT || 3001
