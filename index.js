9a47e7e68e5db6a412858260988d90d82d1f2f8c
Mayank Negi <mayank@Mayanks-MacBook-Air.local>
2024-01-17 23:49:47 +0530

    add JWT auth

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require('express')
 const app = express()
+app.use(express.json())
+
+const jwt = require('jsonwebtoken')
 
 const users = [
     {
@@ -19,9 +22,37 @@ app.get('/', (req, res) => {
     res.send('UP')
 })
 
-app.get('/users', (req, res) => {
-    res.json(users)
+app.get('/users', authToken, (req, res) => {
+    res.json(users.filter(user => req.user.id === user.id))
 })
+
+app.post('/login', (req, res) => {
+    username = req.body.username
+    password = req.body.password
+
+    const user = verifyUserCreds(username, password) 
+    if (user && user.length === 0) {
+        res.sendStatus(200)
+    }
+
+    const token = jwt.sign({id: user[0].id, name: user[0].name}, 'TOKEN_SECRET', {expiresIn: '600s'})
+    res.json({access_token: token})
+})
+
+function verifyUserCreds(username, password) {
+    return users.filter(user => user.name === username && user.password === password)
+}
+
+function authToken(req, res, next) {
+    const token = req.headers['authorization'].split(' ')[1]
+    if (null == token) return res.sendStatus(401)
+
+    jwt.verify(token, 'TOKEN_SECRET', (err, user) => {
+        if (err) return res.sendStatus(403)
+        req.user = user 
+        next()
+    })
+}
 
 const port = process.env.PORT || 3001
 app.listen(port, () => {
