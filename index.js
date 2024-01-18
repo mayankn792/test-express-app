d00f562db1be6657c9965235daea9cefb0775ea9
Mayank Negi <mayank@Mayanks-MacBook-Air.local>
2024-01-19 00:12:54 +0530

    fix typo

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require('express')
-const cors = require('cros')
+const cors = require('cors')
 
 const app = express()
 
 app.use(express.json())
-app.use(cros())
+app.use(cors())
 
 const jwt = require('jsonwebtoken')
 
