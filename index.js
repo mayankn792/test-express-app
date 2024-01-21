7e34afbfe7807b1d0db9eb3140c785d94a7b14e3
Mayank Negi <mayankn792@gmail.com>
2024-01-21 22:20:57 +0530

    add localhost to orgin list

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express()
 app.use(express.json())
 
 const whiteList = {
-    origin: ['https://test-express-app-fe.vercel.app']
+    origin: ['https://test-express-app-fe.vercel.app', 'http://localhost:3000']
 }
 app.use(cors(whiteList))
 
